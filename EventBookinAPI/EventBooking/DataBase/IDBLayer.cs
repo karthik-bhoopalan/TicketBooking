d5fcffdbb9759a0914766f25cabdcdd1bcf86138
karthik-bhoopalan <83977990+karthik-bhoopalan@users.noreply.github.com>
2021-06-08 22:40:45 +0530

    Add files via upload

---
+++ EventBookinAPI/EventBooking/DataBase/IDBLayer.cs
@@ -0,0 +1,18 @@
+﻿using EventBooking.Modal;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventBooking.DataBase
+{
+    public interface IDBLayer
+    {
+        Task<string> ExecuteAsyncSql(SqlParameter[] parameters, string queryString, int outparameterValue);
+        Task<List<T>> ExecuteAsyncTableSql<T>(string procedureName) where T : class;
+
+        Task<List<T>> ExecuteAsyncTableProcedure<T>(SqlParameter[] parameters, string Query) where T : class;
+    }
+}
---
+++ EventBookinAPI/EventBooking/Models/EventInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace EventBooking.Models
+{
+    public partial class EventInfo
+    {
+        public int RecId { get; set; }
+        public string EventId { get; set; }
+        public string EventName { get; set; }
+        public string EventTimings { get; set; }
+        public string EventStatus { get; set; }
+    }
+}
